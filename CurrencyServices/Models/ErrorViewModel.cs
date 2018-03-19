02f6ccc8de9bc5314771f8a042a14bc6d4c2ec62
Grzegorz Brelski <brelski.grzegorz@gmail.com>
2018-03-20 00:52:24 +0100

    Add project files.

---
+++ CurrencyServices/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CurrencyServices.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
